ece42c86885b2785ea569dd70b4e31040a27c6e8
Mattia Franchetto <mt.franchetto@gmail.com>
2017-07-31 18:22:36 +0200

    Implement health check

--- scripts/ClusteredHealthCheck.ts
+++ scripts/ClusteredHealthCheck.ts
@@ -2,19 +2,53 @@ import {IRequest, IRequestHandler, IResponse, Route, IProjectionRegistry} from "
 import {inject} from "inversify";
 import {ICluster} from "./Cluster";
 import {IClusterConfig} from "./ClusterConfig";
+import {Subscription} from "rxjs";
+import {reduce, difference, concat} from "lodash";
+
+type SystemStatus = {
+    members: string[];
+    unreachables: string[];
+    projections: string[];
+}
 
 @Route("/health", "GET")
 class ClusteredHealthCheck implements IRequestHandler {
 
+    private subscription: Subscription;
+    private status: SystemStatus = {
+        members: [],
+        unreachables: [],
+        projections: []
+    };
+
     constructor(@inject("ICluster") private cluster: ICluster,
                 @inject("IProjectionRegistry") private registry: IProjectionRegistry,
                 @inject("IClusterConfig") private clusterConfig: IClusterConfig) {
-
+        this.status.members = this.clusterConfig.nodes;
+        this.status.projections = this.runningProjections();
     }
 
     handle(request: IRequest, response: IResponse) {
-
+        if (!this.subscription) this.subscribeToClusterChanges();
+        response.send(this.status);
     }
+
+    private subscribeToClusterChanges() {
+        this.cluster.changes().subscribe(change => {
+            this.status.members = difference(this.status.members, change.removed);
+            this.status.unreachables = concat(this.status.unreachables, change.removed);
+            this.status.members = concat(this.status.members, change.added);
+            this.status.unreachables = difference(this.status.unreachables, change.added);
+            this.status.projections = this.runningProjections();
+        });
+    }
+
+    private runningProjections(): string[] {
+        return reduce(this.registry.projections(), (result, entry) => {
+            if (this.cluster.canHandle(entry[1].name)) result.push(entry[1].name);
+            return result;
+        }, []);
+    };
 
     keyFor(request: IRequest): string {
         return null;
